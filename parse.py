0f82e46e29df703100b7242c4474f4290de69934
evilmog <dustin.heywood@gmail.com>
2018-12-08 14:00:31 -0700

    updates to format

--- parse.py
+++ parse.py
@@ -4,29 +4,29 @@ f = open('directions_gen.txt', 'r')
 lines = f.readlines()
 
 for line in lines:
-    name = line.rstrip().split(':')[0]
-    exits = line.rstrip().split(':')[1].split(',')
+  name = line.rstrip().split(':')[0]
+  exits = line.rstrip().split(':')[1].split(',')
 
-    exitcode = str(" ")
-    lexit = str("")
+  exitcode = str(" ")
+  lexit = str("")
 
-    for roomexit in exits:
-      if roomexit == 'nw':
-            exitcode = "--northwest 1 "
-      if roomexit == 'n':
-            exitcode = "--north 1 "
-      if roomexit == 'ne':
-            exitcode = "--northeast 1 "
-      if roomexit == 'w':
-            exitcode = "--west 1 "
-      if roomexit == 'e':
-            exitcode = "--east 1 "
-      if roomexit == 'sw':
-            exitcode = "--southwest 1 "
-      if roomexit == 's':
-            exitcode = "--south 1 "
-      if roomexit == 'se':
-            exitcode = "--southeast 1 "
-        lexit += exitcode
+  for roomexit in exits:
+    if roomexit == 'nw':
+      exitcode = "--northwest 1 "
+    if roomexit == 'n':
+      exitcode = "--north 1 "
+    if roomexit == 'ne':
+      exitcode = "--northeast 1 "
+    if roomexit == 'w':
+      exitcode = "--west 1 "
+    if roomexit == 'e':
+      exitcode = "--east 1 "
+    if roomexit == 'sw':
+      exitcode = "--southwest 1 "
+    if roomexit == 's':
+      exitcode = "--south 1 "
+    if roomexit == 'se':
+      exitcode = "--southeast 1 "
+    lexit += exitcode
 
-    print "python room_p.py --area sewer --name " + name + " " + str(lexit)
+  print "python room_p.py --area sewer --name " + name + " " + str(lexit)
