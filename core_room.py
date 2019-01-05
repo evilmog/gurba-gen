ae1bc94855badaa6ae8e3196a24b2aa32f3dae1f
evilmog <dustin.heywood@gmail.com>
2019-01-05 14:03:15 -0700

    updates

--- core_room.py
+++ core_room.py
@@ -261,17 +261,17 @@ if args.obj6 is not None:
 
 monsters = []
 if args.mon1 is not None:
-    objects.append([args.mon1])
+    monsters.append([args.mon1])
 if args.mon2 is not None:
-    objects.append([args.mon2])
+    monsters.append([args.mon2])
 if args.mon3 is not None:
-    objects.append([args.mon3])
+    monsters.append([args.mon3])
 if args.mon4 is not None:
-    objects.append([args.mon4])
+    monsters.append([args.mon4])
 if args.mon5 is not None:
-    objects.append([args.mon5])
+    monsters.append([args.mon5])
 if args.mon6 is not None:
-    objects.append([args.mon6])
+    monsters.append([args.mon6])
 
 if objects:
     roomfile.write('\n  set_objects( \n')
